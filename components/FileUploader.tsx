72eeb34e27d4d9a9717bd0fe25c21ed62f622cc6
rajsingh1301 <rjss2829singh@gmail.com>
2025-11-28 23:49:31 +0530

    build full authentication sysytem

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
+import {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
+    const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div>FileUploader</div>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
   )
 }
 
