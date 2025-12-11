fbd5010f91ab1426418657c67de16a6b25cdb9be
rajsingh1301 <rjss2829singh@gmail.com>
2025-12-11 19:41:19 +0530

    mnnm

--- app/(roots)/[type]/page.tsx
+++ app/(roots)/[type]/page.tsx
@@ -7,6 +7,16 @@ const page = async ({params }: SearchParamProps) => {
     <div className=' mx-auto flex w-full max-w-7xl flex-col items-center gap-8 '>
         <section className='w-full'>
             <h1 className='h1 capitalize'>{type}</h1>
+            <div className='flex mt-2 flex-col justify-between sm:flex-row sm:items-center'> 
+            <p className='text-[16px] leading-5 font-normal'> Total : <span>
+              0 MB</span> </p>
+              <div className='mt-5 flex items-center sm:mt-0 sm:gap-3 '>
+                <p className='text-[16px] leading-5 font-normal hidden sm:block text-[]'>
+
+                </p>
+
+              </div>
+            </div>
              </section>
     </div>
   )
