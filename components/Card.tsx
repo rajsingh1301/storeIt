b5f4b33a8ba93ca9e59ed4ae4f2c1b371a4d2b9c
rajsingh1301 <rjss2829singh@gmail.com>
2025-12-19 23:52:57 +0530

    builing cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,18 @@
+import { Link } from 'lucide-react'
+import { Models } from 'node-appwrite'
 import React from 'react'
+import Thumbnail from './Thumbnail'
 
-function Card() {
+function Card( { file }: { file: Models.Document } ) {
   return (
-    <div>
-      
-    </div>
+   <Link href={file.url} target='_blank' className='flex cursor-pointer flex-col gap-6 rounded-[18px] bg-white p-5 shadow-sm transition-all hover:shadow-drop-3'> 
+   <div className='flex justify-between'>
+    <Thumbnail type={file.type} extension={file.extension}/>
+
+   </div>
+   {file.fileName}
+
+   </Link>
   )
 }
 
